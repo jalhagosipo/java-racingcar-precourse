e45e3abcc351c93a6d44d319f4ea0e4f72c042aa
jalhagosipo <gpfla757@naver.com>
2022-10-06 23:32:03 +0900

    feat: 차의 이름은 5자 이하여야 한다
    - 5자보다 크면 IllegalArgumentException를 발생

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -9,8 +9,21 @@ public class Race {
 
     public Race(String input) {
         String[] names = input.split(",");
+        validateNames(names);
         for (int i = 0; i < names.length; i++) {
             cars.add(new Car(names[i]));
+        }
+    }
+
+    private void validateNames(String[] names) {
+        for (String name : names) {
+            checkNameSize(name);
+        }
+    }
+
+    private void checkNameSize(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 차 이름은 5자 이하여야 합니다.");
         }
     }
 
--- src/test/java/racingcar/RacingcarTest.java
+++ src/test/java/racingcar/RacingcarTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingcarTest {
 
@@ -12,11 +13,22 @@ public class RacingcarTest {
     void n대의_차_이름_입력() {
         String[] names = new String[]{"ping1", "ping2", "ping3"};
         String input = String.join(",", names);
+
         Race race = new Race(input);
         List<Car> cars = race.getCars();
+
         for (int i = 0; i<cars.size(); i++) {
             assertThat(cars.get(i).getName()).isEqualTo(names[i]);
         }
+    }
 
+    @Test
+    void 차의_이름은_5자_이하여야_한다() {
+        String[] names = new String[]{"ping1", "ping2", "ping1123123"};
+        String input = String.join(",", names);
+
+        assertThatThrownBy(() -> new Race(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
     }
 }
