dd37c6be88882d7e0d0303ac2efb076a124cbe0d
jalhagosipo <gpfla757@naver.com>
2022-10-10 18:05:59 +0900

    refactor: Name 클래스 사용
    - 원시값과 문자열 포장을 위해 Name class 사용
    - validation 을 Name에서 함

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -8,17 +8,17 @@ public class Car {
     private final static int MAX_NUMBER = 9;
     private final static int MOVE_FORWARD = 1;
     private final static int MIN_FORWARD_NUMBER = 4;
-    private String name;
+    private Name name;
     private int distance = START_POINT;
     private int randomNumber = pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
 
 
     public Car(String name) {
-        this.name = name;
+        this.name = new Name(name);
     }
 
     public String getName() {
-        return this.name;
+        return this.name.toString();
     }
 
     public int move() {
--- src/main/java/racingcar/RaceGame.java
+++ src/main/java/racingcar/RaceGame.java
@@ -10,21 +10,8 @@ public class RaceGame {
 
     public RaceGame(String input) {
         String[] names = input.split(",");
-        validateNames(names);
         for (int i = 0; i < names.length; i++) {
             cars.add(new Car(names[i]));
-        }
-    }
-
-    private void validateNames(String[] names) {
-        for (String name : names) {
-            checkNameSize(name);
-        }
-    }
-
-    private void checkNameSize(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("[ERROR] 차 이름은 5자 이하여야 합니다.");
         }
     }
 
